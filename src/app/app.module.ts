dd8a572ae14c0def6b4f2f73a21a769c9c56f1e2
LuisGuilh3rme <luislebert01@outlook.com>
2023-10-21 22:41:31 -0300

    feat: add menu-title component template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
+import { MenuTitleComponent } from './components/menu-title/menu-title.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    MenuTitleComponent
   ],
   imports: [
     BrowserModule,
