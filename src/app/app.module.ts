0088fc25467787d9db001e5d493022d6dddd10cc
LuisGuilh3rme <luislebert01@outlook.com>
2023-10-21 22:51:42 -0300

    feat: add menu-bar component template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { MenuTitleComponent } from './components/menu-title/menu-title.component';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    MenuTitleComponent
+    MenuTitleComponent,
+    MenuBarComponent
   ],
   imports: [
     BrowserModule,
